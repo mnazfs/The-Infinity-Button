1216607e65e926d41d416e1dca70d97c97f0c538
Muhammed Naseef <mnaseef2k02@gmail.com>
2025-08-09 01:50:36 +0530

    Edited

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/The-Infinity_Button/'
+  base: '/The-Infinity-Button/'
 })
